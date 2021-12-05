e7c45b1a15d38d04d62f8e3105ee75ac88f58d11
Mohamad Salimi <mohamad.salimi@hotmail.com>
2021-12-05 21:49:47 +0200

    Add files via upload

---
+++ js/database.js
@@ -0,0 +1,55 @@
+const database = [
+    {
+        sectorName: "قطاع السياحة",
+        sectorCode: "t102",
+        sectorIcon: "fas fa-suitcase",
+    },
+    {
+        sectorName: "قطاع الصناعة",
+        sectorCode: "K503",
+        sectorIcon: "fas fa-map-marked-alt",
+    },
+    {
+        sectorName: "قطاع الزراعة",
+        sectorCode: "B201",
+        sectorIcon: "fas fa-suitcase",
+    },
+    {
+        sectorName: "قطاع التجارة",
+        sectorCode: "m907",
+        sectorIcon: "fas fa-map-marked-alt",
+    },
+    {
+        sectorName: "قطاع النقل",
+        sectorCode: "P001",
+        sectorIcon: "fas fa-suitcase",
+    },
+];
+
+const tourism = [
+    {
+        id: "t102",
+        tId: 1,
+        info: "ترخيص عقاري",
+    },
+    {
+        id: "t102",
+        tId: 2,
+        info: "الارشاد السياحي",
+    },
+    {
+        id: "t102",
+        tId: 3,
+        info: "تنظيم الرحلات السياحية",
+    },
+    {
+        id: "t102",
+        tId: 4,
+        info: "حجز وحدات الايواء",
+    },
+    {
+        id: "t102",
+        tId: 5,
+        info: "الايواء السياحي",
+},
+    ];
---
+++ js/sector.js
@@ -0,0 +1,45 @@
+//Select Element passed in
+function selectElement(selector) {
+    return document.querySelector(selector);
+}
+
+//Get The Link
+function getTitle() {
+    let href = document.location.href;
+    let id = href.split('=')[1];
+    for (let i = 0; i < database.length; i++) {
+        if (database[i].sectorCode === id) {
+            selectElement('.title').innerHTML += `${database[i].sectorName}`;
+        }
+    }
+    return id;
+}
+
+function PrintInfo() {
+    let id = getTitle();
+    for (let i = 0; i < tourism.length; i++) {
+        if (tourism[i].id === id) {
+            selectElement(".left").innerHTML += `
+<div class="box">
+<div class="title"><a href="steps.html?id=${tourism[i].tId}">${tourism[i].info}</a></div>
+<a href="steps.html?id=${tourism[i].tId}" class="btn">اختار</a>
+</div>
+`;
+        }
+    }
+}
+
+//Print All Result in the database
+function PrintAll() {
+
+    for (let i = 0; i < database.length; i++) {
+        selectElement('.right').innerHTML += `
+<div class="item">
+<a href="sector.html?id=${database[i].sectorCode}">${database[i].sectorName}</a>
+</div>
+`;
+    }
+}
+
+PrintInfo();
+PrintAll();
