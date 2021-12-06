c6cc2c3e1af0ef7209be74589c8387b28c432b7f
Mohamad Salimi <mohamad.salimi@hotmail.com>
2021-12-06 09:01:47 +0200

    Add files via upload

--- js/database.js
+++ js/database.js
@@ -3,30 +3,45 @@ const database = [
         sectorName: "قطاع السياحة",
         sectorCode: "t102",
         sectorIcon: "fas fa-suitcase",
+        sectorN: "tourism",
+        info: ["ترخيص عقاري", "الارشاد السياحي", "تنظيم الرحلات السياحية", "حجز وحدات الايواء", "الايواء السياحي"],
+        desc: "شرح تفصيلي",
     },
     {
         sectorName: "قطاع الصناعة",
         sectorCode: "K503",
         sectorIcon: "fas fa-map-marked-alt",
+        sectorN: "industry",
+        info: [],
+        desc: "شرح تفصيلي",
     },
     {
         sectorName: "قطاع الزراعة",
         sectorCode: "B201",
         sectorIcon: "fas fa-suitcase",
+        sectorN: "agriculture",
+        info: [],
+        desc: "شرح تفصيلي",
     },
     {
         sectorName: "قطاع التجارة",
         sectorCode: "m907",
         sectorIcon: "fas fa-map-marked-alt",
+        sectorN: "trading",
+        info: [],
+        desc: "شرح تفصيلي",
     },
     {
         sectorName: "قطاع النقل",
         sectorCode: "P001",
         sectorIcon: "fas fa-suitcase",
+        sectorN: "transport",
+        info: [],
+        desc: "شرح تفصيلي",
     },
 ];
 
-const tourism = [
+/*const tourism = [
     {
         id: "t102",
         tId: 1,
@@ -58,3 +73,4 @@ const tourism = [
         desc: "شرح تفصيلي",
 },
     ];
+    */
--- js/sector.js
+++ js/sector.js
@@ -4,30 +4,27 @@ function selectElement(selector) {
 }
 
 //Get The Link
-function getTitle() {
-    let href = document.location.href;
-    let id = href.split('=')[1];
+/*function getInfo() {
+    let parms = new URLSearchParams(document.location.search);
+    let id = parms.get("id");
     for (let i = 0; i < database.length; i++) {
+
         if (database[i].sectorCode === id) {
             selectElement('.title').innerHTML += `${database[i].sectorName}`;
-        }
-    }
-    return id;
-}
-
-function PrintInfo() {
-    let id = getTitle();
-    for (let i = 0; i < tourism.length; i++) {
-        if (tourism[i].id === id) {
-            selectElement(".left").innerHTML += `
+            for (let j = 0; j < database[i].info.length; j++) {
+                let info = database[i].info[j];
+                selectElement(".left").innerHTML += `
 <div class="box">
 <div class="inner-box">
-<div class="title"><a href="steps.html?id=${tourism[i].tId}">${tourism[i].info}</a></div>
-<div class="desc">${tourism[i].desc}</div>
+<div class="title"><a href="steps.html?id=${database[i].sectorCode}&num=${database[i].sectorN}">${info}</a></div>
+<div class="desc">${database[i].desc}</div>
 </div>
-<a href="steps.html?id=${tourism[i].tId}" class="btn">اختار</a>
+<a href="steps.html?id=${database[i].sectorCode}&num=${database[i].sectorN}" class="btn">اختار</a>
 </div>
 `;
+
+            }
+
         }
     }
 }
@@ -37,12 +34,49 @@ function PrintAll() {
 
     for (let i = 0; i < database.length; i++) {
         selectElement('.right').innerHTML += `
+<div class="faq">
 <div class="item">
-<a href="sector.html?id=${database[i].sectorCode}">${database[i].sectorName}</a>
+<a href="sector.html?id=${database[i].sectorCode}">${database[i].sectorName}</a><i class="fas fa-angle-down"></i>
+</div>
+<div class="sub-items">
+${database[i].info.map((inf) => '<div><a href="#">' + inf + '</a></div>').join("")}
+</div>
 </div>
 `;
     }
 }
 
-PrintInfo();
-PrintAll();
+let faqs = document.querySelectorAll(".faq");
+console.log(faqs);
+faqs.forEach(faq => {
+    faq.addEventListener("click", function (e) {
+        selectElement(".sub-items").classList.toggle("active");
+    });
+});
+
+//PrintInfo();
+getInfo();
+PrintAll();*/
+
+function getTitle() {
+    let parms = new URLSearchParams(document.location.search);
+    let id = parms.get("id");
+    for (let i = 0; i < database.length; i++) {
+
+        if (database[i].sectorCode === id) {
+            selectElement('.title').innerHTML += `${database[i].sectorName}`;
+            let sectionName = document.querySelector(`.${database[i].sectorN}`);
+            sectionName.classList.add("active");
+        }
+    }
+}
+
+let faqs = document.querySelectorAll(".box");
+
+faqs.forEach(faq => {
+    faq.addEventListener("click", function (e) {
+        faq.classList.toggle("active");
+    });
+});
+
+getTitle();
